9090f49b3cc822a55b53404f857fdd028e62fa17
joshuamaziarz1-ux <joshua.maziarz1@gmail.com>
2026-08-18 10:21:48 -0400

    Update register service worker to v19

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='mnlt-register-v18';
+const CACHE_NAME='mnlt-register-v19';
 const APP_ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
