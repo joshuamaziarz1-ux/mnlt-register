b2d2a03066b4cd886025ae53893f1f76571e05d8
joshuamaziarz1-ux <joshua.maziarz1@gmail.com>
2026-08-18 14:05:01 -0400

    Bump register cache to v25

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='mnlt-register-v24';
+const CACHE_NAME='mnlt-register-v25';
 const APP_ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
