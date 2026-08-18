77eb33666703f8e6d74645caec1381955163a074
joshuamaziarz1-ux <joshua.maziarz1@gmail.com>
2026-08-18 14:09:51 -0400

    v26: bump service worker cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='mnlt-register-v25';
+const CACHE_NAME='mnlt-register-v26';
 const APP_ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
